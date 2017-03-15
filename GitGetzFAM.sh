ac0dc12c0a6df7a46cb03249f76e51d357610ecd
Rich Jackson <Richard.Jackson@walmart.com>
2017-03-14 21:13:23 -0500

    Initial Commit

---
+++ GitGetzFAM.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+cd /apps/DEV/INFDS/zFAM
+cp -T -U "//'SYS3.CICS.ZCLOUD.IP.zFAM.MD.V010000'" . 2> /dev/null
+cp -T -U "//'SYS3.CICS.ZCLOUD.IP.zFAM.TXT.V010000'" ./txt 2> /dev/null
+cp -T -U "//'SYS3.CICS.ZCLOUD.IP.zFAM.ASM.V010000'" ./asm 2> /dev/null
+cp -T -U "//'SYS3.CICS.ZCLOUD.IP.zFAM.CBL.V010000'" ./cbl 2> /dev/null
+cp -T -U "//'SYS3.CICS.ZCLOUD.IP.zFAM.CPY.V010000'" ./cpy 2> /dev/null
+cp -T -U "//'SYS3.CICS.ZCLOUD.IP.zFAM.EXEC.V010000'" ./exec 2> /dev/null
+cp -T -U "//'SYS3.CICS.ZCLOUD.IP.zFAM.IDCAMS.V010000'" ./idcams 2> /dev/null
+cp -T -U "//'SYS3.CICS.ZCLOUD.IP.zFAM.JCL.V010000'" ./jcl 2> /dev/null
+cp -T -U "//'SYS3.CICS.ZCLOUD.IP.zFAM.MAC.V010000'" ./mac 2> /dev/null
+cp -T -U "//'SYS3.CICS.ZCLOUD.IP.zFAM.RDO.V010000'" ./rdo 2> /dev/null
